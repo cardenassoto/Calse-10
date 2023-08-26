669ace3c07a513db319823b40d0a68838aeefe50
cardenassoto <130591516+cardenassoto@users.noreply.github.com>
2023-08-26 16:49:09 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,29 @@
+// clase 9
+console.log("Aquí estoy a tu servicio");
+
+document.getElementById("button").onclick = ()=>{
+    alert("Aquí estoy a tu servicio")
+}
+
+
+var variable1 = "Aquí estoy a tu servicio";
+
+console.log(variable1);
+
+// Clase 10 Variables
+
+var nombre = "Carmen";
+var apellido = "Cardenas";
+
+
+console.log(nombre + "" + apellido);
+
+var numero1 = 15;
+var numero2 = 12;
+
+console.log(numero1 + numero2);
+
+var variablesuma = 27;
+var numero3 = 30;
+
+console.log(variablesuma+ numero3);
